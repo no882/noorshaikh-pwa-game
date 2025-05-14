322ece0ef74008e9a935f09762ac82f426320a0e
no882 <nomanahmad4181@gmail.com>
2025-05-14 21:53:54 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "noorgame-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
